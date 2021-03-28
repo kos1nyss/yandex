c4add542b1db9270fa5c2c115e2ded4e867213ef
Костя Кондрашов <desanto@inbox.ru>
2021-03-28 12:40:12 +0200

    исправление багов

--- main.py
+++ main.py
@@ -316,4 +316,4 @@ if __name__ == '__main__':
     api.add_resource(AssignOrders, '/orders/assign')
     api.add_resource(CompleteOrders, '/orders/complete')
     api.add_resource(CourierInfo, '/couriers/<int:courier_id>')
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='0.0.0.0', port=8080)
